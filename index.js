ad5159b1277a19900a415dbebc30fd392e744070
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2023-08-30 19:24:12 +0530

    find best dates for sip

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const START_DATE = '2021-01-01';
-const END_DATE = '2021-05-01';
-const SIP_DETAILS = [
-  { day: 1, amount: 5000, index: 'NIFTY200MOMENTM30' },
-];
+const END_DATE = '2023-07-31';
 
 const INVESTMENT_DETAILS_FILE_PATH = 'investment_details.csv';
 
@@ -26,11 +23,16 @@ const getStockPriceFromMap = (stockPricesArray, date) => {
   const stockPrice = stockPricesArray.find(stockPrice => stockPrice.DATE === date);
   if (!stockPrice) {
     const duplicateArray = [...stockPricesArray];
-    const sortedStockPrices = lodash.sortBy(duplicateArray.filter(stockPrice => {
-      return true; // stockPrice.DATE < date; // remove all old dates
-    }), 'DATE');
-    console.error(`Stock price not found for date: ${date}, so I am using ${sortedStockPrices[0].DATE}`);
-    return sortedStockPrices[0].PRICE;
+    let minDiff = Number.MAX_SAFE_INTEGER; let usedRecord;
+    for (const stockRecord of stockPricesArray) {
+      const diff = Math.abs(moment(date).diff(stockRecord.DATE, 'days'));
+      if (diff < minDiff) {
+        minDiff = diff;
+        usedRecord = stockRecord;
+      }
+    }
+    console.error(`Stock price not found for date: ${date}, so I am using ${usedRecord.DATE}`);
+    return usedRecord.PRICE;
   }
   return stockPrice.PRICE;
 };
@@ -85,6 +87,7 @@ const generateInvestmentPattern = async (startDate, endDate, sipDetails) => {
   const indexToReturnsFileMap = await generateReturnFilesMap(indexesToFetch);
 
   const investedMap = {};
+  let toRet = 0;
 
   while (currentDate <= endDate) {
     const indexToStockPricesMap = await getIndexToMonthStockPricesMap(indexesToFetch, currentDate);
@@ -129,6 +132,7 @@ const generateInvestmentPattern = async (startDate, endDate, sipDetails) => {
         GAIN_PERCENTAGE: 100 * (totalGainAtEOM / totalInvestedAtEOM),
       }
     ]);
+    toRet = 100 * totalGainAtEOM/totalInvestedAtEOM;
     
     // make sips for month
     for (const { index, day, amount, } of sipDetails) {
@@ -152,11 +156,42 @@ const generateInvestmentPattern = async (startDate, endDate, sipDetails) => {
     }
     currentDate.setMonth(currentDate.getMonth() + 1);
   }
+  return toRet;
 };
 
 (async () => {
   try {
-    await generateInvestmentPattern(new Date(START_DATE), new Date(END_DATE), SIP_DETAILS);
+    const SIP_DETAILS = [
+      { day: 1, amount: 5000, index: 'NIFTY200MOMENTM30' },
+      { day: 1, amount: 5000, index: 'NIFTY500 VALUE 50' },
+      { day: 1, amount: 5000, index: 'NIFTY 50' },
+    ];
+    let currentDay = 1;
+    let bestDay = 1;
+    let maxGainPercentEnd = 0;
+    let minGainPercentEnd = 100;
+    let worstDay = 1;
+    while (currentDay < 32) {
+      const gainPercentEnd = await generateInvestmentPattern(new Date(START_DATE), new Date(END_DATE), SIP_DETAILS.map(sip => {
+        return {
+          ...sip,
+          day: currentDay,
+        }
+      }));
+      if (gainPercentEnd > maxGainPercentEnd) {
+        maxGainPercentEnd = gainPercentEnd;
+        bestDay = currentDay;
+      }
+      if (gainPercentEnd < minGainPercentEnd) {
+        minGainPercentEnd = gainPercentEnd;
+        worstDay = currentDay;
+      }
+      currentDay++;
+    }
+    return console.log(`
+      Best day: ${bestDay}, Best gain: ${maxGainPercentEnd}
+      Worst day: ${worstDay} Worst gain: ${minGainPercentEnd}
+    `);
   } catch (e) {
     console.error(e);
   }
